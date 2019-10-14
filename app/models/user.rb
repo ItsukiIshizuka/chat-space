6bb4a135940b0585d1bca5362b9c88aa41afce94
ItsukiIshizuka <56192097+ItsukiIshizuka@users.noreply.github.com>
2019-10-14 14:54:19 +0900

    user setting

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   has_many :messages
-  has_many :groupup_users
-  has_many :gros, through: :group_users
+  has_many :group_users
+  has_many :groups, through: :group_users
 end
