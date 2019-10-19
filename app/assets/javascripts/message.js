b6cdd9343b96b4078e89cc410f88d8b957e6afa9
ItsukiIshizuka <56192097+ItsukiIshizuka@users.noreply.github.com>
2019-10-19 19:49:13 +0900

    Add messagefunction
    # Add
    ## Function
    "buildHTML",
    ## Return
     ".done", ".fail"

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,4 +1,38 @@
 $(function(){
+  function buildHTML(message){
+
+    if(message.content == null){
+      var content = ``;
+    }else {
+      var content = `<p class='lower-message__content'>
+                      ${message.content}
+                    </p>`
+    }
+
+    if(message.image !== null){
+      var image = ``
+    }else {
+      var image = `<img class="lower-message__image" src=${message.image.url}>`
+    }
+
+    var html = `<div class='message'>
+                  <div class='upper-message'>
+                    <div class='upper-message__user-name'>
+                    ${message.user_name}
+                    </div>
+                    <div class='upper-message__date'>
+                    ${message.created_at}
+                    </div>
+                  </div>
+                  <div class='lower-message'>
+                    ${content}
+                    ${image}
+                  </div>
+                </div>`
+    return html;
+  }
+
+
   $('.new_message').on('submit', function(e){
     e.preventDefault();
     let formData = new FormData(this);
@@ -11,5 +45,13 @@ $(function(){
       processData: false,
       contentType: false
     })
-  });
+    .done(function(data){
+      let html = buildHTML(data);
+      $('.message').append(html)
+      $('.form__message').val('')
+    })
+    .fail(function(){
+      alert('error');
+    })
+  })
 });
