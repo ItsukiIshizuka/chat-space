6b517931e219c15ac4ebb4f2c43a11c699498b03
ItsukiIshizuka <56192097+ItsukiIshizuka@users.noreply.github.com>
2019-10-21 19:43:46 +0900

    IncrementalSearch comp

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,70 +1,53 @@
 $(function() {
-  //ここからメッセージ非同期
-  // function buildHTML(message){
+  function buildHTML(message){
 
-  //   var content = message.content == null? `` : `<p class='lower-message__content'>${message.content}</p>`;
+    var content = message.content == null? `` : `<p class='lower-message__content'>${message.content}</p>`;
 
-  //   var image = message.image !== null? `` : `<img class="lower-message__image" src=${message.image.url}>`;
+    var image = message.image !== null? `` : `<img class="lower-message__image" src=${message.image.url}>`;
 
-  //   var html = `<div class='message'>
-  //                 <div class='upper-message'>
-  //                   <div class='upper-message__user-name'>
-  //                   ${message.user_name}
-  //                   </div>
-  //                   <div class='upper-message__date'>
-  //                   ${message.created_at}
-  //                   </div>
-  //                 </div>
-  //                 <div class='lower-message'>
-  //                   ${content}
-  //                   ${image}
-  //                 </div>
-  //               </div>`
-  //   return html;
-  // };
+    var html = `<div class='message'>
+                  <div class='upper-message'>
+                    <div class='upper-message__user-name'>
+                    ${message.user_name}
+                    </div>
+                    <div class='upper-message__date'>
+                    ${message.created_at}
+                    </div>
+                  </div>
+                  <div class='lower-message'>
+                    ${content}
+                    ${image}
+                  </div>
+                </div>`
+    return html;
+  };
 
-  // function scroll(){
-  //   $('.message_main').animate({ scrollTop: $('.message_main')[0].scrollHeight}, 1000, 'linear'); //サイト開くと一番下
-  // };
-  // scroll();
+  function scroll(){
+    $('.message_main').animate({ scrollTop: $('.message_main')[0].scrollHeight}, 1000, 'linear'); //サイト開くと一番下
+  };
+  scroll();
 
-  // $('.new_message').on('submit', function(e){
-  //   e.preventDefault();
-  //   let formData = new FormData(this);
-  //   let url = $(this).attr('action');
-  //   $.ajax({
-  //     url: url,
-  //     type: "POST",
-  //     data: formData,
-  //     dataType: 'json',
-  //     processData: false,
-  //     contentType: false
-  //   })
-  //   .done(function(data){
-  //     scroll();
-  //     let html = buildHTML(data);
-  //     $('.message_main').append(html)
-  //     $('form')[0].reset();
-  //   })
-  //   .fail(function(){
-  //     alert('error');
-  //   })
-  // })
-
-  // $('.form__submit').removeAttr('data-disable-with')
-//ここまでメッセージ非同期
-
-//ここからグループ検索インクリメントサーチ
- 
-//ここまでグループ検索インクリメントサーチ
-});
-
-$(function() {
-  $("#user-search-field").on("keyup", function() {
-    var input = $("#user-search-field").val();
-    console.log(input);
+  $('.new_message').on('submit', function(e){
+    e.preventDefault();
+    let formData = new FormData(this);
+    let url = $(this).attr('action');
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      scroll();
+      let html = buildHTML(data);
+      $('.message_main').append(html)
+      $('form')[0].reset();
+    })
+    .fail(function(){
+      alert('error');
+    });
   });
+  $('.form__submit').removeAttr('data-disable-with');
 });
-
-
-
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
 
   def index
+    @users = User.where('name LIKE(?) and id != ?', "%#{params[:input]}%", "#{current_user.id}").limit(20)
     respond_to do |format|
       format.html
       format.json
